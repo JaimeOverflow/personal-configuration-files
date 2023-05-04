04801b5529a9ac8c9038639279fcfa9aefe5a3b7
JaimeOverflow <jaimeoverflow@gmail.com>
2023-05-04 20:12:12 +0100

    Update setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# Kubernetes
+alias k="kubectl"
+alias kgd="kubectl get deployments"
+alias kgp="kubectl get pods"
+alias kgs="kubectl get svc"
+alias kgi="kubectl get ingress"
+alias kl="kubectl logs -f "
+alias ka="kubectl apply -f "
+alias kd="kubectl delete -f "
+alias kdd="kubectl delete deployment "
+alias kwp="watch -n1 kubectl get pods"
+
+# Git
+alias gcm="git checkout main"
+alias gcd="git checkout development"
+
+# Make
+alias m="make "
+
+# Terraform
+alias tfp="terraform fmt; terraform plan"
+alias tfa="terraform fmt; terraform apply"
